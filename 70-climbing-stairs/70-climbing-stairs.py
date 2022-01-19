1d222e7b19ffd98bac37cf114cc5f1537891f762
brianijw <35675623+InjongWon@users.noreply.github.com>
2022-01-19 00:02:30 -0500

    Time: 45 ms (23.67%), Space: 14.2 MB (44.95%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,14 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        def dp(i):
-            if i <= 2: 
-                return i
-            if i not in memo:
-                # Instead of just returning dp(i - 1) + dp(i - 2), calculate it once and then
-                # store the result inside a hashmap to refer to in the future.
-                memo[i] = dp(i - 1) + dp(i - 2)
-            
-            return memo[i]
-        
-        memo = {}
-        return dp(n)
+        if n == 1:
+            return 1
+        dp = [0] * (n+1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
