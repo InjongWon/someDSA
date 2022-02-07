d325f1653ec45fe8b008084c85a249a0b6c459c5
brianijw <35675623+InjongWon@users.noreply.github.com>
2022-02-07 17:28:36 -0500

    Time: 130 ms (5.24%), Space: 14 MB (91.34%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,9 +1,27 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        first_step,second_step = 0, 0
-        minimumCost = [0] * (len(cost) + 1)
-        for i in range(2, len(cost)+1):
-            minimumCost[i] = min(cost[i-2]+ minimumCost[i-2], cost[i-1]+minimumCost[i-1])
+#         minimumCost = [0] * (len(cost) + 1)
+#         for i in range(2, len(cost)+1):
+#             minimumCost[i] = min(cost[i-2]+ minimumCost[i-2], cost[i-1]+minimumCost[i-1])
         
-        return minimumCost[-1]
-                                                        
+#         return minimumCost[-1]
+#Top down(memoization)
+#         def minimum_cost(i):
+#             if i == 0 or i == 1:
+#                 return 0
+#             if i in memo:
+#                 return memo[i]
+#             one_down = minimum_cost(i-1) + cost[i-1]
+#             two_down = minimum_cost(i-2) + cost[i-2]
+#             memo[i] = min(one_down, two_down)
+#             return memo[i]
+        
+#         memo = {}
+#         return minimum_cost(len(cost))
+        downOne, downTwo = 0,0 
+        for i in range(2, len(cost) +1):
+            tmp = downOne
+            downOne = min(downOne + cost[i-1], downTwo + cost[i-2])
+            downTwo = tmp
+        return downOne
+            
