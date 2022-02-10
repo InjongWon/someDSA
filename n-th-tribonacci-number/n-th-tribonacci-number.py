062c9febf92947101a3802df90860cb70e95f755
brianijw <35675623+InjongWon@users.noreply.github.com>
2022-02-10 15:41:00 -0500

    Time: 40 ms, Memory: 13.8 MB - LeetHub

---
+++ n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        F = [0] * 38
+        F[0] = 0
+        F[1] = F[2] = 1
+        for i in range(3, n+1):
+            F[i] = F[i-3] + F[i-2]+ F[i-1]
+        return F[n]
