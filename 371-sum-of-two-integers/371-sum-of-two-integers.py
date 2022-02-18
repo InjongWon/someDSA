95d68258a36d6962913e59d30e0ac9ab3afbf3c6
brianijw <35675623+InjongWon@users.noreply.github.com>
2022-02-18 12:41:44 -0500

    Time: 49 ms (26.39%), Space: 13.8 MB (88.51%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.py
+++ 371-sum-of-two-integers/371-sum-of-two-integers.py
@@ -1,3 +1,51 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        return a+b
+        mask = 0xFFFFFFFF
+		
+        while b != 0:
+            xor = a ^ b
+            xor = xor & mask
+            
+            carry = (a & b) << 1
+            carry = carry & mask
+            a, b = xor, carry
+            
+        max_int = 0x7FFFFFFF
+        if a < max_int:
+            return a
+        else:
+            # if there is overflow
+			# i.e if a is a negative value
+            result = a ^ mask
+            result = ~result
+            return result
+        
+        
+        
+        
+        
+#         public int getSum(int a, int b) {
+#         if(a == 0) {
+#             return b;
+#         }
+        
+#         if(b == 0) {
+#             return a;
+#         }
+        
+#         int carry = 0;
+        
+#         while(b != 0) {
+            
+#             // If both bits are 1, we set the bit to the left (<<1) to 1 -- this is the carry step
+#             carry = (a & b) << 1;
+            
+#             // If both bits are 1, this will give us 0 (we will have a carry from the step above)
+#             // If only 1 bit is 1, this will give us 1 (there is nothing to carry)
+#             a = a ^ b;
+            
+#             b = carry;
+#         }
+        
+#         return a;
+#     }
