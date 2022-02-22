1f1545cb18cef59357987976d3890ef975da3e94
brianijw <35675623+InjongWon@users.noreply.github.com>
2022-02-22 17:21:21 -0500

    Time: 32 ms (79.52%), Space: 13.8 MB (98.85%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.py
+++ 371-sum-of-two-integers/371-sum-of-two-integers.py
@@ -1,4 +1,3 @@
-import math
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         carry = 0
@@ -7,8 +6,7 @@ class Solution:
             carry = (a & b) << 1
             a = a ^ b
             b = carry
-    
-    # for overflow condition like
-    # -1
-    #  1
+        # for overflow condition like
+        # -1
+        #  1
         return a&mask if b > mask else a
