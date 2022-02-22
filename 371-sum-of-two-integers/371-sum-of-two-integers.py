5f295006fb048f0ad78841299851fde59059b0bd
brianijw <35675623+InjongWon@users.noreply.github.com>
2022-02-22 16:26:10 -0500

    Time: 39 ms (54.77%), Space: 13.9 MB (65.27%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.py
+++ 371-sum-of-two-integers/371-sum-of-two-integers.py
@@ -1,51 +1,14 @@
+import math
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        mask = 0xFFFFFFFF
-		
-        while b != 0:
-            xor = a ^ b
-            xor = xor & mask
-            
+        carry = 0
+        mask = 0xffffffff
+        while b & mask != 0:
             carry = (a & b) << 1
-            carry = carry & mask
-            a, b = xor, carry
-            
-        max_int = 0x7FFFFFFF
-        if a < max_int:
-            return a
-        else:
-            # if there is overflow
-			# i.e if a is a negative value
-            result = a ^ mask
-            result = ~result
-            return result
-        
-        
-        
-        
-        
-#         public int getSum(int a, int b) {
-#         if(a == 0) {
-#             return b;
-#         }
-        
-#         if(b == 0) {
-#             return a;
-#         }
-        
-#         int carry = 0;
-        
-#         while(b != 0) {
-            
-#             // If both bits are 1, we set the bit to the left (<<1) to 1 -- this is the carry step
-#             carry = (a & b) << 1;
-            
-#             // If both bits are 1, this will give us 0 (we will have a carry from the step above)
-#             // If only 1 bit is 1, this will give us 1 (there is nothing to carry)
-#             a = a ^ b;
-            
-#             b = carry;
-#         }
-        
-#         return a;
-#     }
+            a = a ^ b
+            b = carry
+    
+    # for overflow condition like
+    # -1
+    #  1
+        return a&mask if b > mask else a
