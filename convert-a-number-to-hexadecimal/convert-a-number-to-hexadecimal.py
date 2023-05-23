19e2b832b3802668d2169aa097ca84b85fd5bcc1
KaiWon <35675623+InjongWon@users.noreply.github.com>
2023-05-23 16:55:26 -0400

    Time: 39 ms, Memory: 16.3 MB - LeetHub

---
+++ convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.py
@@ -0,0 +1,21 @@
+import math
+
+class Solution:
+    def toHex(self, num: int) -> str:
+        if num == 0:
+            return '0'
+    
+        if num < 0:
+            num = (1 << 32) + num
+
+        hex_digits = '0123456789abcdef'  # The hexadecimal digits
+    
+        hex_num = ''  
+    
+        while num > 0:
+            digit = num % 16  
+            hex_digit = hex_digits[digit]  
+            hex_num = hex_digit + hex_num 
+            num //= 16 
+
+        return hex_num
